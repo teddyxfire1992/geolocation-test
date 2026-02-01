ecc8477fa8aada231d56cee687db6c3c057c4e2a
teddyxfire <alen.medo92@gmail.com>
2026-02-01 15:14:22 +0100

    added geolocation package and created a simple app for reproing the getCurrentPosition issue

---
+++ app/index.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import {
+  Button,
+  Switch,
+  Text,
+  View,
+} from 'react-native';
+
+import BackgroundGeolocation, {
+  Location,
+  Subscription
+} from "react-native-background-geolocation";
+
+export default function Index() {
+  const [enabled, setEnabled] = React.useState(false);
+  const [location, setLocation] = React.useState('');
+
+  React.useEffect(() => {
+    /// 1.  Subscribe to events.
+    const onLocation: Subscription = BackgroundGeolocation.onLocation((location) => {
+      console.log('[onLocation]', location);
+      setLocation(JSON.stringify(location, null, 2));
+    })
+
+    const onMotionChange: Subscription = BackgroundGeolocation.onMotionChange((event) => {
+      console.log('[onMotionChange]', event);
+    });
+
+    const onActivityChange: Subscription = BackgroundGeolocation.onActivityChange((event) => {
+      console.log('[onActivityChange]', event);
+    })
+
+    const onProviderChange: Subscription = BackgroundGeolocation.onProviderChange((event) => {
+      console.log('[onProviderChange]', event);
+    })
+
+    /// 2. ready the plugin.
+    BackgroundGeolocation.ready({
+      // Geolocation Config
+      geolocation: {
+        desiredAccuracy: BackgroundGeolocation.DesiredAccuracy.High,
+        distanceFilter: 10,
+        stopTimeout: 5
+      },
+      // Logger Config
+      logger: {
+        debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+        logLevel: BackgroundGeolocation.LogLevel.Verbose
+      },
+      // Application config
+      app: {
+        stopOnTerminate: false,   // <-- Allow the background-service to continue tracking when user closes the app.
+        startOnBoot: true         // <-- Auto start tracking when device is powered-up.
+      },
+    }).then((state) => {
+      setEnabled(state.enabled)
+      console.log("- BackgroundGeolocation is configured and ready: ", state.enabled);
+    });
+
+    return () => {
+      // Remove BackgroundGeolocation event-subscribers when the View is removed or refreshed
+      // during development live-reload.  Without this, event-listeners will accumulate with
+      // each refresh during live-reload.
+      onLocation.remove();
+      onMotionChange.remove();
+      onActivityChange.remove();
+      onProviderChange.remove();
+    }
+  }, []);
+
+  /// 3. start / stop BackgroundGeolocation
+  React.useEffect(() => {
+    if (enabled) {
+      BackgroundGeolocation.start();
+    } else {
+      BackgroundGeolocation.stop();
+      setLocation('');
+    }
+  }, [enabled]);
+
+  return (
+    <View style={{alignItems: 'center'}}>
+      <Text>Click to enable BackgroundGeolocation</Text>
+      <Switch value={enabled} onValueChange={setEnabled}/>
+      <Text style={{fontFamily: 'monospace', fontSize: 12}}>{location}</Text>
+      <Button title="Get current location" onPress={() => {
+        BackgroundGeolocation.getCurrentPosition().then(position => {
+          console.log("getCurrentPosition SUCCESS: ", position)
+        }).catch(error => {
+          console.log("getCurrentPosition ERROR: ", error)
+        })
+      }}/>
+    </View>
+  )
+}
